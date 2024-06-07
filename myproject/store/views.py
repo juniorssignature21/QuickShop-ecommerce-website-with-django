d58176cc72548f20341ffd9d40a216167a1a8db2
Junior's signature <chibuzor.john.2018@gmail.com>
2024-06-07 21:19:52 +0100

    Describe the changes you made

--- myproject/store/views.py
+++ myproject/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -14,7 +14,21 @@ def about(request):
 
 
 def login_user(request):
-    return render(request, 'store/login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been Logged in successfully!!!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error please try again!!!"))
+            return redirect('home')
+    else:
+        return render(request, 'store/login.html', {})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, ("you have been logged out!!!"))
+    return redirect('home')
